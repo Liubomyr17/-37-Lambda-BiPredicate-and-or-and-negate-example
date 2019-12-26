6e62b4060427b2a350c90c298dd0159670ddeb83
L&J Brothers <41821541+Liubomyr17@users.noreply.github.com>
2019-12-26 23:53:04 +0200

    Add files via upload

---
+++ (37)Lambda-BiPredicate and,or and negate example/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.function.BiPredicate;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        BiPredicate<Integer, Integer> biPredicate1 = (x,y)->x>y;
+        BiPredicate<Integer, Integer> biPredicate2 = (x,y)->x-2>y;
+
+        System.out.println(biPredicate1.and(biPredicate2).test(20, 15));
+        System.out.println(biPredicate1.and(biPredicate2).test(10, 8));
+        System.out.println("-----------------------------------------------");
+        System.out.println(biPredicate1.or(biPredicate2).test(10, 15));
+        System.out.println(biPredicate1.or(biPredicate2).test(15, 8));
+
+        System.out.println("-----------------------------------------------");
+
+        boolean test = biPredicate1.negate().test(20, 18);
+        System.out.println(test);
+
+    }
+
+}
